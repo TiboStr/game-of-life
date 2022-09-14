49757bb9c0fc1897971ed05bfeb7d710d5c1856e
Tibo Stroo <tibo.stroo@hotmail.be>
2022-09-14 21:22:57 +0200

    Pass width and height as command line parameters

--- src/main.c
+++ src/main.c
@@ -2,21 +2,29 @@
 
 #include <stdio.h>
 #include <argp.h> /* https://www.linuxtopia.org/online_books/programming_books/gnu_c_programming_tutorial/argp-example.html */
+#include <stdlib.h>
+
+const int DEFAULT_WIDTH = 40;
+const int DEFAULT_HEIGHT = 20;
 
 typedef enum {
-    random, drawing, coordinates
+    random_state, drawing_state, coordinates_state
 } program_states;
 
 
 struct arguments {
     program_states program_state;
     char *infile;
+    int width;
+    int height;
 };
 
 static struct argp_option options[] = {
-        {"random",      'r', 0,      0, "Generate random starting points (default option)"},
-        {"drawing",     'd', "FILE", 0, "Read file where a starting pattern is drawn"},
-        {"coordinates", 'c', "FILE", 0, "Read file where coordinates are listed"},
+        {"random",      'r', 0,        0, "Generate random starting points (default option if nothing is specified)", 0},
+        {"drawing",     'd', "FILE",   0, "Read file where a starting pattern is drawn",                              1},
+        {"coordinates", 'c', "FILE",   0, "Read file where coordinates are listed",                                   1},
+        {"width",       'w', "WIDTH",  0, "The width of the visualization",                                           2},
+        {"height",      'h', "HEIGHT", 0, "The height of the visualization",                                          2},
         {0}
 };
 
@@ -25,18 +33,23 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
 
     switch (key) {
         case 'r':
-            arguments->program_state = random;
+            arguments->program_state = random_state;
             break;
         case 'd':
-            arguments->program_state = drawing;
+            arguments->program_state = drawing_state;
             arguments->infile = arg;
             break;
         case 'c':
-            arguments->program_state = coordinates;
+            arguments->program_state = coordinates_state;
             arguments->infile = arg;
             break;
-        case ARGP_KEY_NO_ARGS:
-            arguments->program_state = random;
+        case 'w':
+            arguments->width = atoi(arg);
+            break;
+        case 'h':
+            arguments->height = atoi(arg);
+            break;
+        case ARGP_KEY_END:
             break;
         default:
             return ARGP_ERR_UNKNOWN;
@@ -52,36 +65,48 @@ int main(int argc, char **argv) {
     struct arguments arguments;
 
     arguments.infile = NULL;
-    arguments.program_state = random;
+    arguments.program_state = random_state;
+    arguments.width = DEFAULT_WIDTH;
+    arguments.height = DEFAULT_HEIGHT;
 
     /* Parse arguments */
     argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
+    printf(
+            "infile: %s\n"
+            "program_state: %i\n"
+            "width: %i\n"
+            "height: %i\n",
+            arguments.infile, arguments.program_state, arguments.width, arguments.height
+    );
 
-    Grid *grid = init_grid(10, 10);
+    if (0) {
+        Grid *grid = init_grid(10, 10);
 
-    printf("start\n");
+        printf("start\n");
 
-    print_grid(grid);
+        print_grid(grid);
 
-    printf("------------------\n");
+        printf("------------------\n");
 
-    next_generation(grid);
-    print_grid(grid);
-
-
-    printf("------------------\n");
+        next_generation(grid);
+        print_grid(grid);
 
 
-    next_generation(grid);
-    print_grid(grid);
+        printf("------------------\n");
 
-    printf("------------------\n");
 
-    next_generation(grid);
-    print_grid(grid);
+        next_generation(grid);
+        print_grid(grid);
 
-    destroy_grid(grid);
+        printf("------------------\n");
 
-    printf("done\n");
+        next_generation(grid);
+        print_grid(grid);
+
+        destroy_grid(grid);
+
+        printf("done\n");
+    }
+
 }
